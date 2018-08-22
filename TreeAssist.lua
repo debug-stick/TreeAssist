b8c73e5d92629b98ec021d981138a9792e2ab64c
Lorain.Li <Lorain.Li@icloud.com>
2018-08-22 11:51:22 +0800

    search log with leaves table

--- TreeAssist.lua
+++ TreeAssist.lua
@@ -1,9 +1,10 @@
 --init a plugin
 --Author Lorain.Li
-g_Plugin=nil;
+g_Plugin = nil
+a_LeavesMap = nil
 function Initialize(a_Plugin)
 	a_Plugin:SetName("TreeAssist")
-	a_Plugin:SetVersion(3)
+	a_Plugin:SetVersion(4)
 	g_Plugin = a_Plugin
 	cPluginManager:AddHook(cPluginManager.HOOK_PLAYER_BREAKING_BLOCK, MyOnPlayerBreakingBlock)
 	LOG("TreeAssist v".. g_Plugin:GetVersion() .." is loaded")
@@ -20,55 +21,47 @@ function MyOnPlayerBreakingBlock(a_Player, a_BlockX, a_BlockY, a_BlockZ, a_Block
 		return false
 	end
 	local a_World = a_Player:GetWorld()
-	local a_Block = a_World:GetBlock(a_BlockX,a_BlockY,a_BlockZ)
-	if(not BlockIsWood(a_Block)) then
-		return
-	end
-	CollectWood(a_World,a_BlockX, a_BlockY, a_BlockZ,a_BlockType, a_BlockMeta)
+	CollectWood(a_World, a_BlockX, a_BlockY, a_BlockZ ,a_BlockType, a_BlockMeta % 4, a_LeavesMap = {})
 end
 
-a_Coord={
-	{-1,0,-1},
-	{-1,0,0},
-	{-1,0,1},
-	{0,0,-1},
-	--{0,0,0},
-	{0,0,1},
-	{1,0,-1},
-	{1,0,0},
-	{1,0,1},
-	{0,1,0},
-	{-1,1,-1},
-	{-1,1,0},
-	{-1,1,1},
-	{0,1,-1},
-	{0,1,1},
-	{1,1,-1},
-	{1,1,0},
-	{1,1,1},
-}
-
-function CollectWood( a_World,a_BlockX,a_BlockY,a_BlockZ,a_BlockType,a_BlockMeta )
-	local  a_Block = a_World:GetBlock(a_BlockX,a_BlockY,a_BlockZ)
-	if((not BlockIsWood(a_Block)) or (a_Block~=a_BlockType)) then
+function CollectWood(a_World, a_BlockX, a_BlockY, a_BlockZ ,a_BlockType, a_BlockMeta, a_LeavesMap)
+	local a_BlockID = a_World:GetBlock(a_BlockX, a_BlockY, a_BlockZ)
+	if(not (BlockIsWood(a_BlockID) or BlockIsLeaves(a_BlockID)) or (a_BlockID ~= a_BlockType)) then
 		return
 	end
-	a_World:DigBlock(a_BlockX, a_BlockY, a_BlockZ)
-	local a_Items = cItems()
-	a_Items:Add(a_BlockType,1,a_BlockMeta % 4)
-	a_World:SpawnItemPickups(a_Items, a_BlockX, a_BlockY, a_BlockZ, math.random())
-	--plant a tree
-	if(a_World:GetBlock(a_BlockX,a_BlockY-1,a_BlockZ) == E_BLOCK_DIRT) then
-		a_World:SetBlock(a_BlockX, a_BlockY, a_BlockZ, E_BLOCK_SAPLING, a_BlockMeta, false)
-	end
-	--check next wood
-	for i=1,17 do
-		if(BlockIsWood(a_World:GetBlock(a_BlockX+a_Coord[i][1],a_BlockY+a_Coord[i][2],a_BlockZ+a_Coord[i][3]))) then
-			CollectWood(a_World,a_BlockX+a_Coord[i][1],a_BlockY+a_Coord[i][2],a_BlockZ+a_Coord[i][3],a_BlockType,a_BlockMeta)
+	if(BlockIsWood(a_BlockID)) then
+		local a_Items = cItems()
+		a_Items:Add(a_BlockType, 1, a_BlockMeta)
+		--collect log
+		a_World:DigBlock(a_BlockX, a_BlockY, a_BlockZ)
+		a_World:SpawnItemPickups(a_Items, a_BlockX, a_BlockY, a_BlockZ, math.random())
+		--plant a sapling
+		if(CanPlantSapling(a_World:GetBlock(a_BlockX, a_BlockY - 1, a_BlockZ))) then
+			a_World:SetBlock(a_BlockX, a_BlockY, a_BlockZ, E_BLOCK_SAPLING, a_BlockMeta, false)
 		end
 	end
+	if(BlockIsLeaves(a_BlockID)) then
+		if(a_LeavesMap[a_BlockX .. ":" .. a_BlockY .. ":" .. a_BlockZ] == true) then
+			return
+		else
+			a_LeavesMap[a_BlockX .. ":" .. a_BlockY .. ":" .. a_BlockZ] = true
+		end
+	end
+	CollectWood(a_World, a_BlockX - 1, a_BlockY, a_BlockZ,a_BlockType, a_BlockMeta, a_LeavesMap)
+	CollectWood(a_World, a_BlockX + 1, a_BlockY, a_BlockZ,a_BlockType, a_BlockMeta, a_LeavesMap)
+	CollectWood(a_World, a_BlockX, a_BlockY, a_BlockZ - 1,a_BlockType, a_BlockMeta, a_LeavesMap)
+	CollectWood(a_World, a_BlockX, a_BlockY, a_BlockZ + 1,a_BlockType, a_BlockMeta, a_LeavesMap)
+	CollectWood(a_World, a_BlockX, a_BlockY + 1, a_BlockZ,a_BlockType, a_BlockMeta, a_LeavesMap)
 end
 
-function BlockIsWood( a_BlockID )
-	return (a_BlockID == E_BLOCK_LOG) or(a_BlockID == E_BLOCK_NEW_LOG)
+function BlockIsWood(a_BlockID)
+	return ((a_BlockID == E_BLOCK_LOG) or (a_BlockID == E_BLOCK_NEW_LOG))
+end
+
+function BlockIsLeaves( a_BlockID )
+	return ((a_BlockID == E_BLOCK_LEAVES) or (a_BlockID == E_BLOCK_NEW_LEAVES))
+end
+
+function CanPlantSapling( a_BlockID )
+	return (a_BlockID == E_BLOCK_DIRT) or (a_BlockID == E_BLOCK_GRASS)
 end
