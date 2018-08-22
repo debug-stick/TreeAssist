a7ea06fe6f504de42bbd2df23329966cb23da29a
Lorain.Li <Lorain.Li@icloud.com>
2018-08-22 09:07:11 +0800

    enum calculate for log

--- TreeAssist.lua
+++ TreeAssist.lua
@@ -20,55 +20,81 @@ function MyOnPlayerBreakingBlock(a_Player, a_BlockX, a_BlockY, a_BlockZ, a_Block
 		return false
 	end
 	local a_World = a_Player:GetWorld()
-	local a_Block = a_World:GetBlock(a_BlockX,a_BlockY,a_BlockZ)
-	if(not BlockIsWood(a_Block)) then
-		return
+	local a_BlockC = a_World:GetBlock(a_BlockX,a_BlockY,a_BlockZ)
+	if(not (BlockIsWood(a_BlockC) or BlockIsLeaves(a_BlockC))) then
+		return false
 	end
-	CollectWood(a_World,a_BlockX, a_BlockY, a_BlockZ,a_BlockType, a_BlockMeta)
+	local m_Block = cItems()
+	m_Block:Add(a_BlockType, 1, a_BlockMeta % 4)
+	for PosY=a_BlockY,a_World:GetHeight(a_BlockX, a_BlockZ),1 do
+		if(not CollectXZWood(a_BlockX,PosY,a_BlockZ,a_World,m_Block)) then
+			break
+		end
+	end
+	a_World:SetBlock(a_BlockX, a_BlockY, a_BlockZ, E_BLOCK_SAPLING, a_BlockMeta, false)
+	return false
 end
 
-a_Coord={
-	{-1,0,-1},
-	{-1,0,0},
-	{-1,0,1},
-	{0,0,-1},
-	--{0,0,0},
-	{0,0,1},
-	{1,0,-1},
-	{1,0,0},
-	{1,0,1},
-	{0,1,0},
-	{-1,1,-1},
-	{-1,1,0},
-	{-1,1,1},
-	{0,1,-1},
-	{0,1,1},
-	{1,1,-1},
-	{1,1,0},
-	{1,1,1},
-}
+function CollectXZWood( x_X,x_Y,x_Z,x_World,m_Block)
+	local a_BlockC = x_World:GetBlock(x_X,x_Y,x_Z)
+	if(not (BlockIsWood(a_BlockC) or BlockIsLeaves(a_BlockC))) then
+		return false
+	end
+	for PosX=x_X,x_X+5,1 do
+		if(not CollectZWood(PosX,x_Y,x_Z,x_World,m_Block)) then
+			break
+		end
+	end
+	for PosX=x_X-1,x_X-5,-1 do
+		if(not CollectZWood(PosX,x_Y,x_Z,x_World,m_Block)) then
+			break
+		end
+	end
+	return true
+end
 
-function CollectWood( a_World,a_BlockX,a_BlockY,a_BlockZ,a_BlockType,a_BlockMeta )
-	local  a_Block = a_World:GetBlock(a_BlockX,a_BlockY,a_BlockZ)
-	if((not BlockIsWood(a_Block)) or (a_Block~=a_BlockType)) then
+function CollectZWood( z_X,z_Y,z_Z ,z_World,m_Block)
+	local a_BlockC = z_World:GetBlock(z_X,z_Y,z_Z)
+	if(not (BlockIsWood(a_BlockC) or BlockIsLeaves(a_BlockC))) then
+		return false
+	end
+	for PosZ = z_Z,z_Z+5,1 do
+		local a_BlockC = z_World:GetBlock(z_X,z_Y,PosZ)
+		if(not (BlockIsWood(a_BlockC) or BlockIsLeaves(a_BlockC))) then
+			break
+		end
+		DigAndSpawnPickup(z_World,z_X,z_Y,z_Z,m_Block)
+	end
+	for PosZ = z_Z-1,z_Z-5,-1 do
+		local a_BlockC = z_World:GetBlock(z_X,z_Y,PosZ)
+		if(not (BlockIsWood(a_BlockC) or BlockIsLeaves(a_BlockC))) then
+			break
+		end
+		DigAndSpawnPickup(z_World,z_X,z_Y,PosZ,m_Block)
+	end
+	return true
+end
+
+function BlockIsWood(a_BlockID)
+	return ((a_BlockID == E_BLOCK_LOG) or (a_BlockID == E_BLOCK_NEW_LOG))
+end
+
+function BlockIsLeaves( a_BlockID )
+	return ((a_BlockID == E_BLOCK_LEAVES) or (a_BlockID == E_BLOCK_NEW_LEAVES))
+end
+
+function DigAndSpawnPickup(a_World, a_BlockX, a_BlockY, a_BlockZ,m_Block)
+	local a_BlockC = a_World:GetBlock(a_BlockX, a_BlockY, a_BlockZ)
+	if(not BlockIsWood(a_BlockC)) then 
 		return
 	end
 	a_World:DigBlock(a_BlockX, a_BlockY, a_BlockZ)
-	local a_Items = cItems()
-	a_Items:Add(a_BlockType,1,a_BlockMeta % 4)
-	a_World:SpawnItemPickups(a_Items, a_BlockX, a_BlockY, a_BlockZ, math.random())
-	--plant a tree
-	if(a_World:GetBlock(a_BlockX,a_BlockY-1,a_BlockZ) == E_BLOCK_DIRT) then
-		a_World:SetBlock(a_BlockX, a_BlockY, a_BlockZ, E_BLOCK_SAPLING, a_BlockMeta, false)
-	end
-	--check next wood
-	for i=1,17 do
-		if(BlockIsWood(a_World:GetBlock(a_BlockX+a_Coord[i][1],a_BlockY+a_Coord[i][2],a_BlockZ+a_Coord[i][3]))) then
-			CollectWood(a_World,a_BlockX+a_Coord[i][1],a_BlockY+a_Coord[i][2],a_BlockZ+a_Coord[i][3],a_BlockType,a_BlockMeta)
-		end
-	end
+	a_World:SpawnItemPickups(m_Block, a_BlockX, a_BlockY, a_BlockZ, math.random())
 end
 
-function BlockIsWood( a_BlockID )
-	return (a_BlockID == E_BLOCK_LOG) or(a_BlockID == E_BLOCK_NEW_LOG)
+function GetSaplingMeta(a_BlockType, a_BlockMeta)
+	if (a_BlockType == E_BLOCK_LOG) then
+		return a_BlockMeta
+	end
+	return a_BlockMeta + 4
 end
